cc7816eca87f04fc18cc27194645cc2461354eec
Thasyo Peres <thasyotp2002@gmail.com>
2023-03-09 13:24:47 -0300

    atualizando progresso

---
+++ 3_FUNDAMENTOS_PART02/fundamentos_part02/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TitleSection03 from './components/TitleSectionThree,';
+
+function App() {
+  return (
+    <div className="App">
+      <TitleSection03/>
+    </div>
+  );
+}
+
+export default App;
---
+++ 3_FUNDAMENTOS_PART02/fundamentos_part02/src/components/TitleSectionThree,.js
@@ -0,0 +1,11 @@
+const TitleSection03 = () => {
+
+    return (
+
+        <h1>Avançando em React!</h1>
+
+    )
+
+}
+
+export default TitleSection03;
