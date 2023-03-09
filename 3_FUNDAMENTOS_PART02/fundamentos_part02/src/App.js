1bc69a60c33d6f09dcfd4939790f77e5c9f227c4
Thasyo Peres <thasyotp2002@gmail.com>
2023-03-09 15:05:20 -0300

    atualizando progresso

--- 3_FUNDAMENTOS_PART02/fundamentos_part02/src/App.js
+++ 3_FUNDAMENTOS_PART02/fundamentos_part02/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import TitleSection03 from './components/TitleSectionThree,';
+import OnePiece from './imgs/wallpaper-hp-one-piece.jpg'
 
 function App() {
   return (
     <div className="App">
       <TitleSection03/>
+      {/* IMAGEM EM PUBLIC - PODEMOS ACESSÁ-LA DIRETAMENTE*/}
+      <div>
+        <img src="/naruto-shippuden-papel-de-parede-2048x1152_49.jpg" alt="naruto" width="500px"/>
+      </div>
+
+      {/* IMAGEM EM UMA PASTA ESPECÍFICA */}
+      <div>
+        <img src={OnePiece} alt="One Piece" width="500px"/>
+      </div>
     </div>
   );
 }
