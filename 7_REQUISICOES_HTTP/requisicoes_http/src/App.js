a105675d0a22cfda569f50e3a9563a13878db03d
Thasyo Peres <thasyotp2002@gmail.com>
2023-05-09 08:41:34 -0300

    atualizando progresso

--- 7_REQUISICOES_HTTP/requisicoes_http/src/App.js
+++ 7_REQUISICOES_HTTP/requisicoes_http/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
+  const addNewProduct = useRef(null);
 
   // RESGATANDO DADOS
   const url = "http://localhost:3000/products";
@@ -23,30 +24,31 @@ function App() {
   }, []);
 
   // ADICIONANDO DADOS
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const product = {
       name,
-      price
-    }
+      price,
+    };
     
-    const newData = async () => {
-      const res = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(product),
-      });
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json"
+      },
+      body: JSON.stringify(product)
+    });
 
-      console.log(res)
-      console.log(product)
-    }
+    //CARREGAMENTO DINÂMICO
+    const addedProduct = await res.json();
 
-    newData();
+    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    setName("");
+    setPrice("");
+    addNewProduct.current.focus();
   }
-
 
   return (
     <div className="App">
@@ -58,15 +60,15 @@ function App() {
           </li>
         ))}
       </ul>
-      <form onClick={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <label>
           Nome
-          <input type="text" name='name' value={name} onChange={(e) => setName(e.target.value)}/>
+          <input type="text" name='name' ref={addNewProduct} value={name} onChange={(e) => setName(e.target.value)}/>
         </label>
 
         <label>
           Preço
-          <input type="text" name='price' value={price} onChange={(e) => setPrice(e.target.value)}/>
+          <input type="number" name='price' value={price} onChange={(e) => setPrice(e.target.value)}/>
         </label>
 
         <input type="submit" value="Criar" id="btn"/>
