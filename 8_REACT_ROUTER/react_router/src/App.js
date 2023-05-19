499bd26f72c3b9223af14303160048b19e1d8e27
Thasyo Peres <thasyotp2002@gmail.com>
2023-05-19 08:52:28 -0300

    atualizando progresso

--- 8_REACT_ROUTER/react_router/src/App.js
+++ 8_REACT_ROUTER/react_router/src/App.js
@@ -5,11 +5,15 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import About from './pages/About';
 
+//Components
+import NavBar from './components/NavBar.js';
+
 function App() {
   return (
     <div className="App">
       <h1>react router</h1>
       <BrowserRouter>
+        <NavBar/>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
