ddb54370612ef84bad83668c6379c19dab5d5166
Thasyo Peres <thasyotp2002@gmail.com>
2023-05-18 23:16:08 -0300

    atualizando progresso

--- 8_REACT_ROUTER/react_router/src/App.js
+++ 8_REACT_ROUTER/react_router/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
+//1- Config react router;
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
   return (
     <div className="App">
       <h1>react router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/about' element={<About/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
