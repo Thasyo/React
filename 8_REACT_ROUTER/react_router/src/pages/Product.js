e51a17e20b8ac0b9e0019659583ae1970e0319bb
Thasyo Peres <thasyotp2002@gmail.com>
2023-05-28 13:15:53 -0300

    atualizando progresso

--- 8_REACT_ROUTER/react_router/src/pages/Product.js
+++ 8_REACT_ROUTER/react_router/src/pages/Product.js
@@ -1,12 +1,26 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import {useFetch} from "../hooks/useFetch";
 
 const Product = () => {
 
+    //ROTAS DINÂMICAS
     const {id} = useParams();
+
+    //CARREGAMENTO INDIVIDUAL DE DADOS COM BASE NAS ROTAS DINÂMICAS
+    const url = `http://localhost:3000/products/${id}`;
+
+    const {data: product, loading, error} = useFetch(url);
 
   return (
     <>
-        <p>ID do produto: {id}</p>
+      {error && <p>Ocorreu um erro...</p>}      
+      {loading && <p>Carregando...</p>}   
+      {product && (
+        <div>
+          <h1>{product.name}</h1>
+          <p>R$ {product.price}</p>
+        </div>
+      )}
     </>
   )
 }
