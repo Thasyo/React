5190353f6ce16e16effedee4d1e5622c12d23a39
Thasyo Peres <thasyotp2002@gmail.com>
2023-05-29 08:57:18 -0300

    atualizando progresso

--- 8_REACT_ROUTER/react_router/src/components/NavBar.js
+++ 8_REACT_ROUTER/react_router/src/components/NavBar.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import "./NavBar.css"
 
 const NavBar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
     </nav>
   )
 }
---
+++ 8_REACT_ROUTER/react_router/src/pages/NotFound.js
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div>
+        <h1><strong>404</strong></h1>
+        <p>Página não encontrada!</p>
+    </div>
+  )
+}
+
+export default NotFound
