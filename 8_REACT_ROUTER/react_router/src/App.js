907fe53bd008bf83136df125a2d72a0c5262fcb9
Thasyo Peres <thasyotp2002@gmail.com>
2023-06-04 13:31:07 -0300

    atualizando progresso

--- 8_REACT_ROUTER/react_router/src/App.js
+++ 8_REACT_ROUTER/react_router/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 //Components
 import NavBar from './components/NavBar.js';
+import SearchForm from './components/SearchForm';
 
 //Pages
 import Home from './pages/Home';
@@ -12,6 +13,7 @@ import About from './pages/About';
 import Product from './pages/Product';
 import Info from './pages/Info';
 import NotFound from './pages/NotFound';
+import {Search} from './pages/Search';
 
 function App() {
   return (
@@ -19,11 +21,13 @@ function App() {
       <h1>REACT ROUTER</h1>
       <BrowserRouter>
         <NavBar/>
+        <SearchForm/>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
           <Route path='/products/:id' element={<Product />} />
           <Route path='/products/:id/info' element={<Info />} />
+          <Route path='/search' element={<Search/>}></Route>
           <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
