b4465d2cf57133b01d73855b16cac89ce934c0a5
Thasyo Peres <thasyotp2002@gmail.com>
2023-06-14 07:15:06 -0300

    atualizando progresso

--- 10_PROJETO_MINIBLOG/miniblog/src/App.js
+++ 10_PROJETO_MINIBLOG/miniblog/src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
+//components
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+
 //pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -9,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <NavBar/>
         <div className="container">
           <Routes>
             <Route path='/' element={<Home/>}/>
@@ -16,6 +21,7 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
