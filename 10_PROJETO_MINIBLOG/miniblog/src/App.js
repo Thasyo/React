ec7989b921d11179510cfe62ff8d7c8f7c3abfd8
Thasyo Peres <thasyotp2002@gmail.com>
2023-06-18 12:18:50 -0300

    atualizando progresso

--- 10_PROJETO_MINIBLOG/miniblog/src/App.js
+++ 10_PROJETO_MINIBLOG/miniblog/src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+import { onAuthStateChanged } from 'firebase/auth';
+
+//hooks
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
 
 //components
 import NavBar from './components/NavBar';
@@ -10,22 +15,42 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
+
+  const [user, setUser] = useState(undefined);
+
+  const {auth} = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  }, [auth]);
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-      <NavBar/>
-        <div className="container">
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
-          </Routes>
-        </div>
-      </BrowserRouter>
-      <Footer/>
+      <AuthProvider value={{user}}>
+        <BrowserRouter>
+        <NavBar/>
+          <div className="container">
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/register' element={<Register/>}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
+        <Footer/>
+      </AuthProvider>
     </div>
   );
 }
