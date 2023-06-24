9479a8acbea8f3c5eec81548000f90ddecba349d
Thasyo Peres <thasyotp2002@gmail.com>
2023-06-24 12:13:37 -0300

    atualizando progresso

--- 10_PROJETO_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
+++ 10_PROJETO_MINIBLOG/miniblog/src/pages/CreatePost/CreatePost.js
@@ -1,5 +1,9 @@
 import styles from './CreatePost.module.css'
 import {useState} from 'react';
+import {useNavigate} from "react-router-dom"
+import {useAuthValue} from "../../context/AuthContext"
+import {useInsertDocument} from "../../hooks/useInsertDocument"
+
 
 const CreatePost = () => {
 
@@ -7,19 +11,52 @@ const CreatePost = () => {
   const [image, setImage] = useState("");
   const [body, setBody] = useState("");
   const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const navigate = useNavigate();
+
+  const {user} = useAuthValue()
+
+  const {insertDocument, response} = useInsertDocument("posts");
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setFormError("");
 
-    const contentPost = {
+    //validar url da imagem
+    try {
+      new URL(image)
+    } catch (error) {
+      setFormError("A imagem precisa ser uma URL.")
+    }
+
+    //criar o array de tags
+    const tagsArray = tags.split(",").map((tag) => (
+      tag.trim().toLowerCase()
+    ))
+
+    //checar todos os valores
+    if(!title || !image || !tags || !body){
+      setFormError("Por favor, preencha todos os campos!");
+    }
+
+    if(formError){
+      return;
+    }
+
+    insertDocument({
       title,
       image,
       body,
-      tags
-    }
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
 
-    console.log(contentPost);
-  }
+    //redirecionar para a página inicial.
+    navigate("/");
+
+  };
 
   return (
     <div className={styles.create_post}>
@@ -47,11 +84,12 @@ const CreatePost = () => {
           <span>Tags</span>
           <input type="text" name='tags' required placeholder="Separe as tags por vírgula" onChange={(e) => setTags(e.target.value)} value={tags}/>
         </label>
-        <button className='btn'>Criar</button>
-        {/* {!loading && <button className='btn'>Entrar</button>}
-        {loading && <button className='btn' disabled>Aguarde...</button>}
-        {error && <p className='error'>{error}</p>}
-        {sucess && <p className='sucess'>{sucess}</p>} */}
+
+        {!response.loading && <button className='btn'>Criar Post</button>}
+        {response.loading && <button className='btn' disabled>Aguarde...</button>}
+        {response.error && <p className='error'>{response.error}</p>}
+        {formError && <p className='error'>{formError}</p>}
+
       </form>
 
     </div>
