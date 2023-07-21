1c3e6b829d34e604cc9e3718939e642f60430c7b
Thasyo Peres <thasyotp2002@gmail.com>
2023-07-21 15:56:02 -0300

    atualizando progresso

---
+++ 11_HOOKS/hooks_react/src/hooks/usePrevious.js
@@ -0,0 +1,16 @@
+import { useEffect, useRef, useDebugValue } from "react"
+
+export const usePrevious = (value) => {
+
+    const ref = useRef()
+
+    useDebugValue("--- CUSTOM HOOK E USEDEBUGVALUE ---")
+    useDebugValue("O valor anterior é: " + value)
+
+    useEffect(() => {
+        ref.current = value
+    })
+
+    return ref.current
+
+}
